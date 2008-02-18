589420d067f6b771214f32fdd39b32d09bb43c7d
alecu <>
2008-02-18 05:01:18 +0000

    primera version del decompresor

--- armado/compresor.py
+++ armado/compresor.py
@@ -19,48 +19,52 @@ Header: pickle de una lista de tuplas (nombre_articulo, origen, tamanio)
 Articulos, uno detras del otro
 """
 
-# recorrer todos los nombres de articulos, y ordenarlos en un dict por su numero de bloque, segun el hash
-fileNames = []
-for root, dirs, files in os.walk(PATH):
-	for fileName in files:
-		fileNames.append( (root, fileName) )
-		if len(fileNames)%10000 == 0:
-			print "encontrados %d articulos" % len(fileNames)
+def generar():
+	# recorrer todos los nombres de articulos, y ordenarlos en un dict por su numero de bloque, segun el hash
+	fileNames = []
+	for root, dirs, files in os.walk(PATH):
+		for fileName in files:
+			fileNames.append( (root, fileName) )
+			if len(fileNames)%10000 == 0:
+				print "encontrados %d articulos" % len(fileNames)
 
-print "procesando", len(fileNames), "articulos"
-numBloques= len(fileNames) / ARTICLES_PER_BLOCK
-bloques = {}
-for root, fileName in fileNames:
-	bloqNum = hash(fileName)%numBloques
-	if bloqNum not in bloques:
-		bloques[bloqNum] = []
-	bloques[bloqNum].append((root,fileName))
-
-# recorrer el dict de bloques, juntando cada uno
-for bloqNum, fileNames in bloques.items():
-	print "procesando bloque", bloqNum, "de", numBloques
-
-	# armo el header
-	header=[]
-	origin = 0
+	print "procesando", len(fileNames), "articulos"
+	numBloques= len(fileNames) / ARTICLES_PER_BLOCK
+	bloques = {}
 	for root, fileName in fileNames:
-		fullName = path.join(root, fileName)
-		size = path.getsize(fullName)
-		header.append( (fullName, origin, size) )
-		origin += size
-	headerBytes = pickle.dumps(header)
-	
-	# abro el archivo a comprimir
-	bloqName = "%08x"%bloqNum
-	f = compressor("salida/bloques/%s.cdp"%bloqName, "wb")
+		bloqNum = hash(fileName)%numBloques
+		if bloqNum not in bloques:
+			bloques[bloqNum] = []
+		bloques[bloqNum].append((root,fileName))
 
-	# grabo la longitud del header, y el header
-	f.write( struct.pack("<l", len(headerBytes) ) )
-	f.write( headerBytes )
-	
-	# grabo cada uno de los articulos
-	for root, fileName in fileNames:
-		fullName = path.join(root, fileName)
-		f.write(open( fullName ).read())
+	# recorrer el dict de bloques, juntando cada uno
+	for bloqNum, fileNames in bloques.items():
+		print "procesando bloque", bloqNum, "de", numBloques
 
-	f.close()
+		# armo el header
+		header=[]
+		origin = 0
+		for root, fileName in fileNames:
+			fullName = path.join(root, fileName)
+			size = path.getsize(fullName)
+			header.append( (fileName, origin, size) )
+			origin += size
+		headerBytes = pickle.dumps(header)
+		
+		# abro el archivo a comprimir
+		bloqName = "%08x"%bloqNum
+		f = compressor("salida/bloques/%s.cdp"%bloqName, "wb")
+
+		# grabo la longitud del header, y el header
+		f.write( struct.pack("<l", len(headerBytes) ) )
+		f.write( headerBytes )
+		
+		# grabo cada uno de los articulos
+		for root, fileName in fileNames:
+			fullName = path.join(root, fileName)
+			f.write(open( fullName ).read())
+
+		f.close()
+
+if __name__ == "__main__":
+	generar()
---
+++ armado/decompresor.py
@@ -0,0 +1,34 @@
+import os
+import struct
+import pickle
+from os import path
+from gzip import GzipFile as compressor
+#from bz2 import BZ2File as compressor
+
+numBloques = 113
+from compresor import ARTICLES_PER_BLOCK
+
+"""
+Formato del bloque:
+
+4 bytes: Longitud del header
+Header: pickle de una lista de tuplas (nombre_articulo, origen, tamanio)
+	(el origen es 0 despues del header)
+
+Articulos, uno detras del otro
+"""
+
+def getArticle(fileName):
+	bloqNum = hash(fileName)%numBloques
+	bloqName = "%08x"%bloqNum
+	f = compressor("salida/bloques/%s.cdp"%bloqName, "rb")
+	headerSize = struct.unpack("<l", f.read(4))[0]
+	headerBytes = f.read(headerSize)
+	header = pickle.loads(headerBytes)
+	for name, origin, size in header:
+		if name == fileName:
+			f.seek(4+headerSize+origin)
+			return f.read(size)
+
+if __name__ == "__main__":
+	print getArticle("Queso_de_Vidiago_cbab.html")
