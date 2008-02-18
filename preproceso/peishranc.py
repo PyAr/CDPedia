cb5ea28a0dd89dc47123b0b94f977c52a2453f62
facundobatista <>
2008-02-18 19:46:07 +0000

    > Nueva version del peishranc.py, hecha por Piuke.

--- preproceso/peishranc.py
+++ preproceso/peishranc.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
+#para probar este modulo, hay que pasarle el path entre comillas dobles. Con las comillas simples me daba error por lo menos en windows. 
+#Asi sería "c:/cdpedia/accesorios/wikifiles/...."
+import sys, os, subprocess, re, urllib, codecs
+from os.path import join, getsize
 
-import sys, os, subprocess, re, urllib
-
-"""
-	Metricas a aplicar para determinar el valor de una pagina.
-	1.Cantidad de referencias. Ver como se pondera despues.
-	2.Tamanio de la pagina. Ver como se pondera despues.
-"""
 usage = """
 Usar: peishranc.py <nombre_archivo>
 
@@ -18,9 +15,10 @@ Usar: peishranc.py <nombre_archivo>
 """
 
 PASOSHOW = 1000
-CARAJO = open('c:/wikipediaOffLine/Error.txt', 'w')
+CARAJO = open('/dev/null', 'w')
 
 def getLineas(nomarch):
+    #Ya no es necesario, dado que cambio la forma de entrada. No es mas un 7z, sino que vamos a trabajar con los archivos ya descomprimidos.
     p = subprocess.Popen(("7z l %s" % nomarch).split(), stdout=subprocess.PIPE)
     util = False
     for lin in p.stdout:
@@ -39,8 +37,8 @@ def getLineas(nomarch):
         fname = os.path.basename(lin[-1])
         yield (tamanio, fname)
 
-
 class GetArchs(object):
+    #Ya no es necesario, dado que cambio la forma de entrada. No es mas un 7z, sino que vamos a trabajar con los archivos ya descomprimidos.
     def __init__(self, nomarch):
         self.p = subprocess.Popen(("7z e -so %s" % nomarch).split(), stdout=subprocess.PIPE, stderr=CARAJO)
 
@@ -48,53 +46,69 @@ class GetArchs(object):
         return self.p.stdout.read(largo)
 
 reenlac = re.compile("\.\.\/.*?\.html")
-#reenlac = re.compile("def .*:")
 
-def getReferencias(objLineas, nomarch, acum):
-    print "Analizando referencias %r..." % nomarch
-    pasoant = 0
-    total = 0
-    garchs = GetArchs(nomarch)
-    for (tamanio, nombre) in objLineas:
-        # vamos mostrando de a tantos
-        total += 1
-        if total // PASOSHOW > pasoant:
-            sys.stdout.write("\r%d...     " % total)
-            sys.stdout.flush()
-            pasoant = total // PASOSHOW
-        arch = garchs(tamanio)
-        for enlace in reenlac.findall(arch):
-            enlace = urllib.unquote(enlace).split('/')      #con el split y el [-1] de la linea de abajo, lo que hacemos es sacar la mugre de las referencias absolutas de una pagina.
-            acum[enlace[-1]] = acum.get(enlace[-1], 0) + 1
-        if total > 10000:
-            return
-    return
+def getReferencias(path):
+    #Esta funcion devuelve HASTA AHORA (TODO hay que derivarlo a un archivo de texto) un diccionario con nombre de articulo y cant. de referencias que tiene el mismo.
+    acum={}
+    f = open("preproceso/refList.txt","w")
+    print 'Se comienza a contar las referencias, teniendo en cuenta la carpeta: ' + path
+    for dirpath, dirs, archs in os.walk(path):
+        break
+        for nombre in archs:
+            for enlace in reenlac.findall(file(join(dirpath, nombre)).read()):
+                enlace = urllib.unquote(enlace).split('/')
+                acum[enlace[-1]] = acum.get(enlace[-1], 0) + 1
+    tam={}
+    print 'Se comienza a buscar el peso de los archivos, teniendo en cuenta la carpeta: ' + path
+    for dirpath, dirs, archs in os.walk(path):
+        for nombre in archs:
+            if not nombre in acum.keys():
+                acum[nombre] = 0
+            acum[nombre] = (acum[nombre],getsize(join(dirpath, nombre)))
+#        print type(tam),"TAMANIOTAMANIOTAMANIO"            
+    #print acum
+#                f.write(str(enlace)+','+str(acum[enlace][0])+','+str(acum[enlace][1])+'\n')
+#                print enlace,'PPPPPPPPPPPPPP'
+#    f.write(str(acum.items()[0]) + ',' + str(acum.items()[1]) + '\n')
+#    print acum
+    for key,value in acum.iteritems():
+        info= "%s,%s" % (key,value)
+        f.write(info+"\n")
+#    f.write(str(acum.items())+'\n')
+    f.close()
+    #esto habria que bajarlo a un archivo de texto, para que no sea necesario volver a procesar cuando se quieran aplicar estas metricas.
 
-def main(nomarch):
-    total = 0
-    lineas = getLineas(nomarch) #De aca se va a obtener nombre y tamanio del articulo
-    acum = {}
-    getReferencias(lineas, nomarch, acum)   #De aca se va a obtener nombre y cantidad de referencias.
-    dicFinal = {}
+def getTamanios(path):
+    #Esta funcion devuelve HASTA AHORA (TODO hay que derivarlo a un archivo de texto) un diccionario con nombre de articulo y tamanio del mismo.
+    tam={}
+    print 'Se comienza a buscar el peso de los archivos, teniendo en cuenta la carpeta: ' + path
+    for dirpath, dirs, archs in os.walk(path):
+        for nombre in archs:
+            tam[nombre] = getsize(join(dirpath, nombre))
+        print type(tam),"TAMANIOTAMANIOTAMANIO"
+        
+    #esto habria que bajarlo a un archivo de texto, para que no sea necesario volver a procesar cuando se quieran aplicar estas metricas.
+
+def main(path):
+    getReferencias(path)
+    #getTamanios(path)
+
+    """
     for (tamanio, nombre) in lineas:
         dicFinal[nombre] = (acum.get(nombre,0),tamanio)
     dicFinal = filter(lambda x:(x[1][0] > 1000), dicFinal.items())
     #dicFinal = sorted(dicFinal, key=lambda x: x[1][1], reverse=True)
     dicFinal.sort(key=lambda x: x[1][1], reverse=True)
+   
     i=0
     while (total < 600*1024*1024) and i < len(dicFinal):
         total += dicFinal[i][1][1]
         print dicFinal[i]
         i+=1
-        
+    """
 
-#def filterReferencias():
-#    return (x[1][0] > 5)
-   
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print usage
         sys.exit(1)
-
     main(sys.argv[1])
-
