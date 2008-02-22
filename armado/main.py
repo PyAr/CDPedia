499e8af92294aaa1a625a637e418f03acb839d6e
alecura <>
2008-02-22 17:47:25 +0000

    Mover autorun.inf y CDPedia.ico al directorio salida Corregida la pagina inicial

--- armado/main.py
+++ armado/main.py
@@ -8,6 +8,6 @@ def runServer():
 
 thread.start_new(runServer, ())
 time.sleep(3)
-#webbrowser.open("http://localhost:8000/Universidad.html")
-subprocess.call(r"win32\prism\prism.exe -id cdpedia@python.com.ar -uri http://localhost:8000/Universidad.html -status off -location off -scrollbars on".split())
+#webbrowser.open("http://localhost:8000/Portal%7EPortada_9ada.html")
+subprocess.call(r"win32\prism\prism.exe -id cdpedia@python.com.ar -uri http://localhost:8000/Portal%7EPortada_9ada.html -status off -location off -scrollbars on".split())
 print "terminado, saliendo."
