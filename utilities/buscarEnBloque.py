ca51be6830888338012cc2356c3bdd3abad053fe
facundobatista <>
2009-03-21 23:39:14 +0000

    Le podemos pasar -v para que nos de el articulo.

--- utilities/buscarEnBloque.py
+++ utilities/buscarEnBloque.py
@@ -4,18 +4,39 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
+import optparse
+
 from src.armado import compresor
 
-def main(nomart):
+def main(nomart, verbose):
     _art_mngr = compresor.ArticleManager(verbose=True)
     info = _art_mngr.getArticle(nomart)
-    print "Largo artículo devuelto", len(info)
+    if info is None:
+        print "No se encontró el artículo"
+    else:
+        print "Largo artículo devuelto", len(info)
+        if verbose:
+            print "Artículo:\n", repr(info)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Usar:  buscarEnBloque.py nombre_articulo"
-        sys.exit()
+    msg = u"""
+  buscarEnBloque.py [-v] nombre_articulo
+"""
 
-    main(sys.argv[1])
+    parser = optparse.OptionParser()
+    parser.set_usage(msg)
+    parser.add_option("-v", "--verbose", action="store_true",
+                  dest="verbose", help="muestra info de lo que va haciendo")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.print_help()
+        exit()
+
+    nomart = args[0]
+    verbose = bool(options.verbose)
+
+    main(nomart, verbose)
 
