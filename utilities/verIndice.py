9f222038026cb6ebd8742f00a78f9a2b176094fd
facundobatista <>
2009-03-21 22:49:56 +0000

    Al verIndice.py se le puede pasar una o más palabras para que las busque.

--- utilities/verIndice.py
+++ utilities/verIndice.py
@@ -10,16 +10,23 @@ sys.path.append(os.path.abspath("."))
 
 from src.armado.cdpindex import Index
 
-def main(arch_ind):
+def main(arch_ind, palabras):
     indice = Index(arch_ind)
-    indice.listar()
+
+    if not palabras:
+        indice.listar()
+    else:
+        encontrado = indice.search(*palabras)
+        for it in encontrado:
+            print it
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usar:  verIndice.py <indice_base>"
+    if len(sys.argv) < 2:
+        print "Usar:  verIndice.py <indice_base> [palabra [...]]"
         print "           donde el indice_base es la parte base del nombre,"
         print "           tienen que estar ambos .ids y .words"
+        print "           Las palabras opcionales son buscadas en el índice"
         sys.exit()
 
     base = sys.argv[1]
@@ -29,4 +36,6 @@ if __name__ == "__main__":
     if not os.access("%s.words" % base, os.R_OK):
         print "No se encontró el archivo '%s.words'" % base
         sys.exit()
-    main(base)
+
+    palabras = [x.decode("utf8") for x in sys.argv[2:]]
+    main(base, palabras)
