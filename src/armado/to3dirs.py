5e21848ad4d705d190bbcaf5ae61c2c2d1aa36d2
dmascialino@gmail.com <>
2011-02-05 20:25:14 +0000

    Agrego script que genera listado de los articulos que estan en determinados namespaces.

--- src/armado/to3dirs.py
+++ src/armado/to3dirs.py
@@ -17,6 +17,10 @@ def to_path(pagina):
     """
     if not pagina:
         raise ValueError
+
+    if ':' in pagina:
+        pagina = pagina[pagina.find(':')+1:]
+
     pagina = _escape_dir(pagina)
     dirs = []
     if len(pagina) == 1:
@@ -31,8 +35,13 @@ def to_path(pagina):
 def to_complete_path(pagina):
     return os.path.join(to_path(pagina), to_filename(pagina))
 
-to_filename = _escape_filename
-
+def to_filename(pagina):
+    if ':' in pagina:
+        namespace = pagina[:pagina.find(':')+1]
+        pagina = pagina[pagina.find(':')+1:]
+    else:
+        namespace = ''
+    return namespace + _escape_filename(pagina)
 
 if __name__ == "__main__":
     assert os.path.join(u"*", NULL, NULL) == to_path(u"*/")
@@ -42,4 +51,7 @@ if __name__ == "__main__":
     assert os.path.join(u"$", u"9", NULL) == to_path(u"$9.99")
 
     assert u"*"+ BARRA == to_filename(u"*/")
+    assert u"Anexo:*"+ BARRA == to_filename(u"Anexo:*/")
+    assert os.path.join(u"a", u"b", u"c") == to_path(u"Anexo:abcdefgh")
 
+    assert os.path.join(u'a',u':',u'b') == to_path(u'Anexo:a:blanco')
---
+++ utilities/listar_articulos_en_namespaces.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Navega las paginas que listan los articulos por Namespace para armar un listado
+que luego puede ser utilizado por scraper.py para conseguir los articulos
+"""
+
+import urllib2
+from functools import partial
+from BeautifulSoup import BeautifulSoup
+import re
+
+UA = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.10) Gecko/20100915 Ubuntu/10.04 (lucid) Firefox/3.6.10'
+
+WIKI = 'http://es.wikipedia.org'
+TODAS = '/wiki/Especial:Todas/'
+SPACES = ['Wikipedia:', 'Anexo:', 'Portal:']
+
+req = partial(urllib2.Request, data = None, headers = {'User-Agent': UA})
+
+def guardar_listado(soup, archivo):
+    t = soup.findAll('table', {'class':'mw-allpages-table-chunk'})[0]
+    titulos = [l['title'] for l in t.findAll('a')]
+    archivo.write('\n'.join(titulos).encode('utf-8'))
+    archivo.write('\n')
+
+def siguiente_link(soup):
+    a = soup.find('a', text=re.compile("^Siguiente p\xe1gina \("))
+    if a:
+        return a.findParent('a')['href']
+
+def traer_pagina(link):
+    html = urllib2.urlopen(req(link)).read()
+    return BeautifulSoup(html)
+
+if __name__ == '__main__':
+    fh = open('articulos_en_namespaces.txt','w')
+    for space in SPACES:
+        soup = traer_pagina(WIKI + TODAS + space)
+        guardar_listado(soup, fh)
+        next_link = siguiente_link(soup)
+        while next_link:
+            print next_link
+            soup = traer_pagina(WIKI + next_link)
+            guardar_listado(soup, fh)
+            next_link = siguiente_link(soup)
+
+    fh.close()
+
