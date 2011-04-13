097a4f7cbecee110c17eebb07123736322d05e6f
dmascialino <>
2011-04-13 01:48:15 +0000

    cambio chiquito en el script para generar el exe

--- resources/autorun.win/mkautorun.bat
+++ resources/autorun.win/mkautorun.bat
@@ -1,5 +1,5 @@
 cd win32main
 python setup.py py2exe
 cd ..
-copy win32main\dist\*.* cdroot\win32\main
+copy win32main\dist\*.* cdroot\cdpedia\win32\main
 rem tools\mkisofs -v -V autorun -o demo.iso -R -J cdroot 
