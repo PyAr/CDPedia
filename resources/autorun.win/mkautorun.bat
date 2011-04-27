03dc09995d6f98515c728f4f1dc2c4445bfc9430
dmascialino@gmail.com <>
2011-04-27 23:56:00 +0000

    Ahora win32main.exe se crea con Python2.5 . issue #102

--- resources/autorun.win/mkautorun.bat
+++ resources/autorun.win/mkautorun.bat
@@ -1,5 +1,5 @@
 cd win32main
-python setup.py py2exe
+c:\python25\python.exe setup.py py2exe
 cd ..
 copy win32main\dist\*.* cdroot\cdpedia\win32\main
 rem tools\mkisofs -v -V autorun -o demo.iso -R -J cdroot 
