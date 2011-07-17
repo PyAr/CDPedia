3fc57df87f836e767d392bc3ef9978ba379ceb19
gringotumadre <>
2011-07-17 20:55:09 +0000

    Agrego metodo from_path a to3dirs.

--- src/armado/to3dirs.py
+++ src/armado/to3dirs.py
@@ -53,19 +53,10 @@ def to_complete_path(pagina):
     #FIXME: Si se quiere generar en Windows se debe cambiar '/' por '\'
     return '/'.join((to_path(pagina), to_filename(pagina)))
 
+def from_path(path):
+    """ Quita los 3 dirs del path """
+    path = to_pagina(path)
+    return path[6:]
+
+
 to_filename = _escape_filename
-
-if __name__ == "__main__":
-    assert os.path.join(u"*", NULL, NULL) == to_path(u"*/")
-    assert os.path.join(u"a", u"b", u"c") == to_path(u"abcdefgh")
-    assert os.path.join(u"á", NULL, NULL) == to_path(u"á")
-    assert os.path.join(u"á", u"þ", NULL) == to_path(u"áþ")
-    assert os.path.join(u"$", u"9", NULL) == to_path(u"$9.99")
-
-    assert u"*"+ BARRA == to_filename(u"*/")
-    assert u"Anexo:*"+ BARRA == to_filename(u"Anexo:*/")
-    assert os.path.join(u"a", u"b", u"c") == to_path(u"Anexo:abcdefgh")
-
-    assert os.path.join(u'a',u':',u'b') == to_path(u'Anexo:a:blanco')
-    assert os.path.join(u'N',u'o',u'e') == to_path(u'Noestoy:Anexo:a:blanco')
-    assert BARRA + BARRA + u':Tr3s.Jeans' == to_filename(u'//:Tr3s.Jeans')
