e4c542d2cd32b4e95639325b3029a530cfb709e8
facundobatista@gmail.com <>
2012-02-24 23:10:42 +0000

    Fixed header

--- cdpedia.py
+++ cdpedia.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-# -- coding: utf8 --
+# -*- coding: utf8 -*-
+
+# Copyright 2006-2012 CDPedistas (see AUTHORS.txt)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  http://code.google.com/p/cdpedia/
+
 
 import os
 import sys
