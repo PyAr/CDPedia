605407e73ee19a644eea9d1cfcb91be24e82031b
Iara <alfaroasadiara@gmail.com>
2020-05-04 16:42:34 -0300

    Add the translations.py

---
+++ src/web/translations.py
@@ -0,0 +1,4 @@
+from gettext import gettext as _
+
+MONTHS = [_('January'), _('February'), _('March'), _('April'), _('May'), _('June'), _('July'), _('August'),
+          _('September'), _('October'), _('November'), _('December')]
