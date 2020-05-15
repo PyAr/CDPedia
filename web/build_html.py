f51c8d3ab3b48a35cd0bb083fe6336410dbc44aa
berelarsenp <berelarsenp@gmail.com>
2020-05-15 20:20:14 -0300

    Eliminar yaml y agregar variables al .py

--- web/build_html.py
+++ web/build_html.py
@@ -6,9 +6,7 @@ from jinja2 import Template  # fades
 with open('index_template.es.html', 'rt', encoding='utf8') as fh:
     template = Template(fh.read())
 
-variables_values = yaml.safe_load(open('./values_es.yaml'))
-
-rendered = template.render(variables_values)
+rendered = template.render(version='0.8.4', month_year='Junio 2017')
 
 with open("index.es.html", "wt", encoding="utf8") as fh:
     fh.write(rendered)
