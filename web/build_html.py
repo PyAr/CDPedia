963840aff43419bd1dcf1cf026b39dffdd80db7b
berelarsenp <berelarsenp@gmail.com>
2020-05-23 00:18:15 -0300

    Incluir jinja2 en 'requirements.txt', terminar script que construye los index.htm de cada template de lenguaje existente en 'templates', incluir el template en inglés y unificar el estilo de html.

--- web/build_html.py
+++ web/build_html.py
@@ -1,12 +1,21 @@
-#!/usr/bin/fades
+import os
+from jinja2 import Template
 
-import yaml  # fades
-from jinja2 import Template  # fades
+templates = 'templates'
 
-with open('index_template.es.html', 'rt', encoding='utf8') as fh:
-    template = Template(fh.read())
+for lang_template in os.listdir(templates):
 
-rendered = template.render(version='0.8.4', month_year='Junio 2017')
+    language = lang_template.split('.')[1]
 
-with open("index.es.html", "wt", encoding="utf8") as fh:
-    fh.write(rendered)
+    with open(os.path.join(templates, lang_template), 'rt', encoding='utf8') as fh:
+        template = Template(fh.read())
+
+    if language == 'es':
+        rendered = template.render(version='0.8.4', month_year='Junio 2017')
+    elif language == 'en':
+        rendered = template.render(version='0.8.4', month_year='June 2017')
+
+    index_name = f'index.{language}.html'
+
+    with open(index_name, 'wt', encoding='utf8') as fh:
+        fh.write(rendered)
