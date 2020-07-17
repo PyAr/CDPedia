60184e89d9f8292b6592b9e850143e8757fd6709
Maria Andrea Vignau <mavignau@protonmail.com>
2020-07-17 00:36:50 +0000

    Utilities to create and search the index, sqlite vs compressed.

---
+++ utilities/create_index.py
@@ -0,0 +1,205 @@
+# -*- encoding: utf8 -*-
+
+# Copyright 2009-2020 CDPedistas (see AUTHORS.txt)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  https://github.com/PyAr/CDPedia/
+import os
+import sys
+sys.path.append(os.path.abspath(os.curdir))
+
+import argparse
+import pathlib
+import base64
+import logging
+import operator
+import re
+import os
+import shutil
+from logging.handlers import RotatingFileHandler
+
+import config
+from src.armado import to3dirs
+from src.armado.sqlite_index import Index as IndexSQL
+from src.armado.compressed_index import Index as IndexComp
+from src.armado.cdpindex import filename2words, normalize_words
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+formatter = logging.Formatter("%(asctime)s  %(name)-20s %(levelname)-8s %(message)s")
+handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler("cdpetron.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger = logging.getLogger("index")
+
+WORDS = re.compile(r"\w+", re.UNICODE)
+PATH_TEMP = pathlib.Path("./temp")
+PATH_IDX = pathlib.Path("./idx")
+
+def calculate():
+    """Calculate the HTMLs with more score and store both lists."""
+    all_pages = []
+    colsep = config.SEPARADOR_COLUMNAS
+    with PATH_TEMP.joinpath("page_scores_final.txt").open("rt", encoding='utf8') as fh:
+        for line in fh:
+            page, score = line.strip().split(colsep)
+            dir3, fname = to3dirs.get_path_file(page)
+            all_pages.append((dir3, fname, int(score)))
+
+    # order by score, and get top N
+    all_pages.sort(key=operator.itemgetter(2), reverse=True)
+    return all_pages
+
+def generate_from_html(verbose=True):
+    """Creates the index. used to create new versions of cdpedia."""
+    # make redirections
+    redirs = {}
+    with PATH_TEMP.joinpath("redirects.txt").open("rt", encoding='utf8') as fh:
+        for line in fh:
+            orig, dest = line.strip().split(config.SEPARADOR_COLUMNAS)
+
+            # in the original article, the title is missing
+            # so we use the words founded in the filename
+            # it isn't the optimal solution, but works
+            words, title = filename2words(orig)
+            redirs.setdefault(dest, []).append((words, title))
+
+    top_pages = calculate()
+
+    titles_texts = {}
+    with PATH_TEMP.joinpath("titles.txt").open("rt", encoding='utf8') as fh:
+        for line in fh:
+            arch, title, encoded_primtext = line.strip().split(config.SEPARADOR_COLUMNAS)
+            primtext = base64.b64decode(encoded_primtext).decode("utf8")
+            titles_texts[arch] = (title, primtext)
+
+    def genSQL():
+        """Source generator to SQLite index."""
+        for dir3, arch, score in top_pages:
+            # auxiliar info
+            namhtml = os.path.join(dir3, arch)
+            title, primtext = titles_texts[arch]
+            logger.debug("Adding to index: [%r]  (%r)" % (title, namhtml))
+
+            # give the title's words great score: 50 plus
+            # the original score divided by 1000, to tie-break
+            ptje = 50 + score // 1000
+            words = list(WORDS.findall(normalize_words(title)))
+            yield words, ptje, (namhtml, title, True, primtext)
+            # pass words to the redirects which points to
+            # this html file, using the same score
+            if arch in redirs:
+                for (words, title) in redirs[arch]:
+                    yield words, ptje, (namhtml, title, False, "")
+
+    def genComp():
+        """Source generator to compressed index."""
+        for dir3, arch, score in top_pages:
+            # auxiliar info
+            namhtml = os.path.join(dir3, arch)
+            title, primtext = titles_texts[arch]
+            logger.debug("Adding to index: [%r]  (%r)" % (title, namhtml))
+
+            # give the title's words great score: 50 plus
+            # the original score divided by 1000, to tie-break
+            ptje = 50 + score // 1000
+            for word in WORDS.findall(normalize_words(title)):
+                yield word, (namhtml, title, ptje, True, primtext)
+
+            # pass words to the redirects which points to
+            # this html file, using the same score
+            if arch in redirs:
+                for (words, title) in redirs[arch]:
+                    for word in words:
+                        yield word, (namhtml, title, ptje, False, "")
+
+    return len(top_pages), genSQL, genComp
+
+
+def ptjes(source):
+    """Show ptjes from source."""
+    ptjes = {}
+    for words, ptje, (namhtml, title, redir, primtext) in source:
+        k = ptje // 1000
+        ptjes[k] = ptjes.get(k,0) + 1
+    print(ptjes)
+
+
+def comp(source):
+    """Compares title vs generated html path."""
+    n = 0
+    r = 0
+    rr = 0
+    for words, ptje, (namhtml, title, ppal, primtext) in source:
+        # auxiliar info
+        tt = title.replace(" ", "_")
+        # tt = normalize_words(tt)
+        tt = tt[0].upper() + tt[1:]
+        #dir3 = '/'.join(tt[:3])
+        dir3, arch = to3dirs.get_path_file(tt)
+        expected = os.path.join(dir3, arch)
+
+        if not ppal:
+            rr +=1
+        if namhtml != expected:
+            if ppal:
+                n += 1
+                print("-->", namhtml, "\n  `", expected)
+            else:
+                r += 1
+    print("No armables::", n, "   Redirs diferentes::", r, "  Redirs::", rr)
+
+
+
+if __name__ == "__main__":
+    # main()
+    help = f"""Creates index files.
+
+    Default path is '{PATH_IDX}' for index files,
+    and '{PATH_TEMP}' for the scrapped files to index."""
+
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument('-c', '--comp', dest='indexes', action='append_const',
+                        const=IndexComp, help="construct compressed index")
+    parser.add_argument('-s', '--sql', dest='indexes', action='append_const',
+                        const=IndexSQL, help="construct sqlite index")
+    args = parser.parse_args()
+    if not args.indexes:
+        print("No index selected!")
+        sys.exit(1)
+    if PATH_IDX.exists():
+        if IndexSQL in args.indexes:
+            sqlitepath = PATH_IDX.joinpath("index.sqlite")
+            if sqlitepath.exists():
+                sqlitepath.unlink()
+                print(f"Database index {str(sqlitepath)} was removed")
+        if IndexComp in args.indexes:
+            for file in PATH_IDX.glob("comp*"):
+                file.unlink()
+                print(f"Database index {str(file)} was removed")
+    else:
+        PATH_IDX.mkdir()
+
+    n_pag, genSQL, genComp = generate_from_html()
+    #comp(genSQL())
+    #gen()
+    # ptjes(genSQL())
+    if IndexSQL in args.indexes:
+        idx = IndexSQL.create(str(PATH_IDX), genSQL())
+    if IndexComp in args.indexes:
+        idx = IndexComp.create(str(PATH_IDX), genComp())
---
+++ utilities/search_index.py
@@ -0,0 +1,102 @@
+# -*- encoding: utf8 -*-
+
+# Copyright 2009-2020 CDPedistas (see AUTHORS.txt)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  https://github.com/PyAr/CDPedia/
+
+import os
+import sys
+import argparse
+import timeit
+from pprint import pprint as pp
+sys.path.append(os.path.abspath(os.curdir))
+
+from src.armado.sqlite_index import Index as IndexSQL
+from src.armado.compressed_index import Index as IndexComp
+
+
+def show_results(result, verbose):
+    res = list(result)
+    print("Resultados:", len(res), "Time: ", timeit.default_timer() - initial_time)
+    if args.verbose:
+        pp(res)
+    return [tuple(r[0:2]) for r in res]
+
+
+def show(title, data, other):
+    n = 0
+    p = []
+    for item in data:
+        if not item in other:
+            print("     ", item)
+            n +=1
+        else:
+            pos = other.index(item)
+            if pos in p:
+                print("repetido:", item)
+            p.append(other.index(item))
+    print(title," Total:", n, "   data:", len(data), " other:", len(other))
+    print("-" * 40)
+    return n
+
+
+if __name__ == "__main__":
+    PATH_IDX = "./idx"
+    help = f"""Search some words and compare results in different indexes.
+
+    Uses {PATH_IDX} as path to index."""
+
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument('keys', metavar='word', type=str, nargs='+',
+                        help='strings to search')
+    parser.add_argument('-c', '--comp', dest='indexes', action='append_const',
+                        const=IndexComp, help="use compressed index")
+    parser.add_argument('-s', '--sql', dest='indexes', action='append_const',
+                        const=IndexSQL, help="use sqlite index")
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        default=False, help="Show detailed results")
+    parser.add_argument('-w', '--word', dest='complete', action='store_true',
+                        default=False, help="complete word search")
+    parser.add_argument('-p', '--partial', dest='partial', action='store_true',
+                        default=False, help="partial word search")
+    parser.add_argument('-d', '--difference', dest='diff', action='store_true',
+                        default=False, help="differences in search's results")
+    args = parser.parse_args()
+    if not args.indexes:
+        args.indexes = [IndexSQL, IndexComp]
+    if not args.complete and not args.partial:
+        args.complete = True
+        args.partial = True
+    res_part = [0] * 2
+    res_comp = [0] * 2
+    for nro, Index in enumerate(args.indexes):
+        initial_time = timeit.default_timer()
+        idx = Index(PATH_IDX)
+        print(repr(Index))
+        print("Open Time: ", timeit.default_timer() - initial_time)
+        if args.complete:
+            initial_time = timeit.default_timer()
+            res = idx.search(args.keys)
+            res_comp[nro] = show_results(res, args.verbose)
+        if args.partial:
+            initial_time = timeit.default_timer()
+            res = idx.partial_search(args.keys)
+            res_part[nro] = show_results(res, args.verbose)
+
+    if args.diff:
+        show("Complete word: in sql not in comp", res_comp[0], res_comp[1])
+        show("Complete word: in comp not in sql", res_comp[1], res_comp[0])
+        show("Partial word: in sql not in comp", res_part[0], res_part[1])
+        show("Partial word: in comp not in sql", res_part[1], res_part[0])
