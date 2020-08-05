7c347d235adb75d75296ee1f306531cc5af0acfb
Facundo Batista <facundo@taniquetil.com.ar>
2020-08-05 17:47:40 -0300

    Couple of small fixes after real life scraping.

--- src/scraping/scraper.py
+++ src/scraping/scraper.py
@@ -205,9 +205,12 @@ class WikipediaArticle(object):
         return self._history
 
     def iter_history_json(self, json_rev_history):
-        pages = json_rev_history['query']['pages']
+        pages = json_rev_history['query'].get('pages')
+        if pages is None:
+            raise PageHaveNoRevisionsError("Page without history")
+
         assert len(pages) == 1
-        pageid = list(pages.keys())[0]
+        (pageid,) = pages.keys()
         if pageid == '-1':
             # page deleted / moved / whatever but not now..
             raise PageHaveNoRevisionsError("Bad value for pageid")
@@ -240,7 +243,7 @@ class WikipediaArticle(object):
         self.acceptance_delta = datetime.timedelta(acceptance_days)
         idx, hist = self.iterate_history()
         if idx != 0:
-            logger.info("Possible vandalism (idx=%d) in %r", idx, self.basename)
+            logger.debug("Possible vandalism (idx=%d) in %r", idx, self.basename)
         return self.get_revision_url(hist.page_rev_id)
 
     def iterate_history(self):
