f70b425253799fa7622c31934439d3a8d4feb913
Federico Zuccolo <federico.zuccolo@gmail.com>
2021-01-18 23:44:37 -0300

    Fix copyright header

--- src/web/test_infra.py
+++ src/web/test_infra.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2020 CDPedistas (see AUTHORS.txt)
+# Copyright 2020 CDPedistas (see AUTHORS.txt)
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
